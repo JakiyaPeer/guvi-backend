c9b755384945f93a7075033ad554fc757f3e5fcc
jakiyasafiya123 <jakiyapeer@gmail.com>
2024-12-10 16:49:55 +0530

    first commit

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-import bcrypt from "bcrypt";
+
 
 const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
@@ -14,13 +14,7 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-// Hash password before saving
-UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
-});
 
-const User=mongoose.model("User",UserSchema)
+
+const User=mongoose.model("users",UserSchema)
 export default User;
